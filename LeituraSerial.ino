9c02c987faec98feb3c528ca00c2803d266b06ce
PHPGomes <pedrohpgomes@gmail.com>
2025-08-26 17:00:50 -0300

    dell

--- LeituraSerial.ino
+++
@@ -1,9 +0,0 @@
-void setup() {
-  Serial.begin(115200);
-}
-
-void loop() {
-  float angulo = obterAnguloEncoder(); // sua função para ler o encoder
-  Serial.println(angulo); // envia para o PC
-  delay(50); // taxa de atualização
-}
